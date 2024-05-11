69ad42a5eb0b884d4f4738b8de522ec10ffd4873
gmontesaa <158535358+gmontesaa@users.noreply.github.com>
2024-05-11 12:01:30 -0500

    agregar nombre a clase main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void Main(String[]args){
+        imprimirnombre();
+    }
+    public static void imprimirnombre(){
+        System.out.println("Assigment 3 por Geronimo Montes");
+    }
+    
+}
